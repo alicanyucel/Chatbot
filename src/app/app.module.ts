bb0bad072fc3b459b5866544058edd3f988022d7
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-09-10 15:53:35 +0300

    sorun çözüldü

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'; // Burada düzeltme yaptım
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatbotComponent } from './components/chatbot/chatbot.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ChatbotService } from './services/chat.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatbotComponent
+    ChatbotComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule 
   ],
-  providers: [],
+  providers: [ChatbotService], //
   bootstrap: [AppComponent]
 })
 export class AppModule { }
